16b940e72d47eead2083c05de53114d2b535adb1
Dont <declan@dontatme.home>
2022-08-12 21:42:11 -0400

    Added more Utils

---
+++ src/compiler/utils/full_char_code_at.rs
@@ -0,0 +1,10 @@
+pub fn full_char_code_at(str: &str, i: usize) -> u8 {
+    let code = &str[i..i+1].as_bytes()[0];
+    
+    if *code <= 0xd7ff || *code >= 0xe000 {
+        return *code
+    }
+
+    let next = &str[i + 1..i+2].as_bytes()[0];
+    (code << 10) + next - 0x35fdc00
+}
--- src/compiler/utils/mod.rs
+++ src/compiler/utils/mod.rs
@@ -2,3 +2,10 @@ mod names;
 mod namespaces;
 mod patterns;
 mod trim;
+mod get_code_frame;
+mod full_char_code_at;
+
+
+//re-exports
+pub use get_code_frame::get_code_frame;
+pub use full_char_code_at::full_char_code_at;
