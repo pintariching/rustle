8a566f6e51cd612a1325a839857051301aa2e15e
Tilen Pintarič <tilen.pintaric@aviko.si>
2022-08-13 21:37:27 +0200

    added structs to interfaces.rs

--- src/compiler/parse/index.rs
+++ src/compiler/parse/index.rs
@@ -1,1 +1,20 @@
+use crate::compiler::interfaces::TemplateNode;
 
+struct LastAutoClosedTag {
+    tag: String,
+    reason: String,
+    depth: i32,
+}
+
+pub struct Parser {
+    template: String,
+    filename: Option<String>,
+    custom_element: bool,
+    index: i32,
+    stack: Vec<TemplateNode>,
+    html: Fragment,
+    css: Vec<Style>,
+    js: Vec<Script>,
+    meta_tags: Vec<String>,
+    last_auto_closed_tag: Option<LastAutoClosedTag>,
+}
