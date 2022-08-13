b910141bcc9918108a8b60bf26aa82615ba1231c
Declan <60301669+qwertydelle@users.noreply.github.com>
2022-08-13 11:56:07 -0400

    Update src/compiler/utils/full_char_code_at.rs
    Co-authored-by: Khang Nguyen Duy <40488299+iceghost@users.noreply.github.com>

--- src/compiler/utils/full_char_code_at.rs
+++ src/compiler/utils/full_char_code_at.rs
@@ -1,11 +1,4 @@
 //Named full_char_code_at in javascript compiler
-pub fn word(str: &str, i: usize) -> u8 {
-    let code = &str[i..i+1].as_bytes()[0];
-    
-    if *code <= 0xd7ff || *code >= 0xe000 {
-        return *code
-    }
-
-    let next = &str[i + 1..i+2].as_bytes()[0];
-    (code << 10) + next - 0x35fdc00
+pub fn full_char_code_at(str: &str, i: usize) -> u32 {
+    str.chars().nth(i).unwrap() as u32
 }
