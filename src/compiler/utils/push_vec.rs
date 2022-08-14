0104ed347dbcad23688a4b8093e330ac0a5f20d7
pintariching <64165058+pintariching@users.noreply.github.com>
2022-08-14 14:03:00 +0200

    Update src/compiler/utils/push_vec.rs
    Co-authored-by: Khang Nguyen Duy <40488299+iceghost@users.noreply.github.com>

--- src/compiler/utils/push_vec.rs
+++ src/compiler/utils/push_vec.rs
@@ -1,7 +1,5 @@
 // Pushes all `items` into `vec` using `push`, therefore mutating the vec.
 
 fn push_vec<T>(vec: &mut Vec<T>, items: Vec<T>) {
-    for item in items {
-        vec.push(item);
-    }
+    vec.extend(items);
 }
