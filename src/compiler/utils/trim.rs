1abca389b9a8c23a6f2a98bb93e375af8dd6ee1a
Tilen Pintarič <tilen.pintaric@aviko.si>
2022-08-14 13:46:31 +0200

    fixed some code so it builds

--- src/compiler/utils/trim.rs
+++
@@ -1,9 +0,0 @@
-use super::patterns::{END_WHITESPACE, START_WHITESPACE};
-
-pub fn trim_start(str: &str) -> &str {
-    &START_WHITESPACE.replace(str, "")
-}
-
-pub fn trim_end(str: &str) -> &str {
-    &END_WHITESPACE.replace(str, "")
-}
