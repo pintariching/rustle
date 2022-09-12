97539dcf3fe1dc9272c326808c3126f3f904ba49
pintariching <tilen.pintaric99@gmail.com>
2022-09-12 13:10:31 +0200

    test: :white_check_mark: add regex tests for compiler::utils::patterns

--- rustle/src/compiler/utils/patterns.rs
+++ rustle/src/compiler/utils/patterns.rs
@@ -8,3 +8,53 @@ lazy_static! {
     pub static ref START_NEWLINE: Regex = Regex::new("^\r?\n").unwrap();
     pub static ref DIMENSIONS: Regex = Regex::new("^(?:offset|client)(?:Width|Height)$").unwrap();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{DIMENSIONS, END_WHITESPACE, START_NEWLINE, START_WHITESPACE, WHITESPACE};
+
+    #[test]
+    fn test_whitespace_regex() {
+        let samples = vec![" ", "\t", "\r", "\n", "	"];
+
+        for s in samples {
+            assert!(WHITESPACE.is_match(s));
+        }
+    }
+
+    #[test]
+    fn test_start_whitespace_regex() {
+        let samples = vec![" word", "\tword", "\rword", "\nword", "	word"];
+
+        for s in samples {
+            assert!(START_WHITESPACE.is_match(s));
+        }
+    }
+
+    #[test]
+    fn test_end_whitespace_regex() {
+        let samples = vec!["word ", "word\t", "word\r", "word\n", "	word"];
+
+        for s in samples {
+            assert!(END_WHITESPACE.is_match(s));
+        }
+    }
+
+    #[test]
+    fn test_start_newline_regex() {
+        let samples = vec!["\rword", "\nword"];
+
+        for s in samples {
+            assert!(END_WHITESPACE.is_match(s));
+        }
+    }
+
+    #[test]
+    fn test_dimensions_regex() {
+        let samples = vec!["offsetWidth", "offsetHeight", "clientWidth", "clientHeight"];
+
+        for s in samples {
+            assert!(DIMENSIONS.is_match(s));
+        }
+    }
+}
